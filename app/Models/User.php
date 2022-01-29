ddd8e0c2a30b0439f5b89019ecb0a7e5e55d7076
theafolayan <theafolayan@gmail.com>
2022-01-29 12:55:03 +0100

    installed bavix multi wallet package

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Bavix\Wallet\Traits\HasWallet;
+use Bavix\Wallet\Traits\HasWallets;
+use Bavix\Wallet\Interfaces\Wallet;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasWallet, HasWallets;
 
     /**
      * The attributes that are mass assignable.
